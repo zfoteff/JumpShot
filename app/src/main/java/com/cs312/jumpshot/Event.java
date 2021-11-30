835026c1a07b84f7bd0b560dbe9a360e0fd28f47
Macy Ryan <macyryan@Macys-MacBook-Pro.local>
2021-11-30 15:28:27 -0800

    Event class added

---
+++ app/src/main/java/com/cs312/jumpshot/Event.java
@@ -0,0 +1,53 @@
+package com.cs312.jumpshot;
+
+public class Event {
+
+    private long id; 
+    String eventName;
+    String startTime;
+    String location;
+    String photoFileName;
+
+    public Event() {
+
+        id = -1;
+        eventName = "EMPTY NAME";
+        startTime = "EMPTY START";
+        location = "EMPTY LOCATION";
+        photoFileName = "placeholderimage";
+
+    }
+
+    public Event(long id, String eventName, String startTime, String location, String photoFileName) {
+
+        this.id = id;
+        this.eventName = eventName;
+        this.startTime = startTime;
+        this.location = location;
+        this.photoFileName = photoFileName;
+
+    }
+
+    public long getId() {return id; }
+    public void setId(long newId) { id = newId; }
+    public String getEventName() {
+        return eventName;
+    }
+    public void setEventName(String newEventName) { eventName = newEventName; }
+    public String getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(String newStartTime) { startTime = newStartTime; }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String newLocation) { location = newLocation; }
+    public String getPhotoFileName() {
+        return photoFileName;
+    }
+    public void setPhotoFileName(String newPhotoFileName) { photoFileName = newPhotoFileName; }
+
+    @Override
+    public String toString() { return "Event Name: " + eventName + ", Start Time: " + startTime +
+            ", Location: " + location + ", Photo File Name: " + photoFileName; }
+}
