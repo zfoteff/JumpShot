5ed90d0e8797c75af21fc74d51dd4b84103bc46a
zfoteff <zfoteff@zagmail.gonzaga.edu>
2021-11-30 16:29:57 -0800

    created notification builder and logic

--- app/src/main/java/com/cs312/jumpshot/MainActivity.java
+++ app/src/main/java/com/cs312/jumpshot/MainActivity.java
@@ -1,14 +1,51 @@
 package com.cs312.jumpshot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String CHANNEL_ID = "JumpShot";
+    NotificationManager notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openNotificationChannel();
+        Button notificationButton = findViewById(R.id.notificationButton);
+
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.stadium)
+                        .setContentTitle("Blank")
+                        .setContentText("Blank")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                notificationManager.notify(0, notificationBuilder.build());
+            }
+        });
+    }
+
+    private void openNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_desc);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
